64a3e042c852ddfa335217f2a1624f3c680ba90c
dyc <dingyangcai@163.com>
2023-07-04 22:43:30 +0800

    ss

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,18 @@ import mockrequests from "./mockAjax.js";
 
 // 三级联动接口 git 无参数
 // 发请求： axios返回的结果是promise
-export const reqCategoryList = () => {return requests.get(`/product/getBaseCategoryList`)}
+export const reqCategoryList = () => {
+  return requests.get(`/product/getBaseCategoryList`);
+};
 // 获取banner轮播图数据
-export const reqGetBannerList = () => {return mockrequests.get('/banner')}
+export const reqGetBannerList = () => {
+  return mockrequests.get("/banner");
+};
 // 获取floor小轮播图数据
-export const reqFloorList = () => {return mockrequests.get('/floor')}
+export const reqFloorList = () => {
+  return mockrequests.get("/floor");
+};
+// 搜索商品接口
+export const reqGetSearchInfo = (params) => {
+  return requests({ url: "/list", method: "POST", data: params });
+};
--- src/store/search/index.js
+++ src/store/search/index.js
@@ -1,25 +1,33 @@
 // search 小仓库
 
 // 引入请求接口函数
-
-
+import { reqGetSearchInfo } from "@/api";
 
 const state = {
-
-}
+    // 初始化仓库地址，根据数据类型绝定
+    searchList: {}
+};
 const mutations = {
-
-}
+    GETSEARCHLIST(state, searchList){
+        state.searchList = searchList;
+    }
+};
 const actions = {
-
-}
-const getters = {
-
-}
+  // 获取搜索模块的数据
+  async getSearchList({ commit }, params = {}) {
+    const dyc = await reqGetSearchInfo(params);
+    // console.log(dyc);
+    if(dyc.code == 200){
+        commit('GETSEARCHLIST', dyc.data);
+    }
+  },
+};
+// 计算属性
+const getters = {};
 // 暴露
 export default {
-    state,
-    mutations,
-    actions,
-    getters
-}
+  state,
+  mutations,
+  actions,
+  getters,
+};
